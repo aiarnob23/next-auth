87f1881c12413dae58a26981d60f5eb313516254
Aminul Islam Arnob <aiarnob23@gmail.com>
2024-12-03 08:38:33 +0600

    Update authOptions.ts

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,6 +1,7 @@
-import  { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { cookies } from "next/headers";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -19,7 +20,26 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/login",
   },
+
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google" || account?.provider === "github") {
+        // You can log the account details or perform checks here
+        console.log("Account provider:", account?.provider);
+        console.log("Profile:", profile);
+        if (profile?.email) {
+          (await cookies()).set({
+            name: 'Manual Token',
+            value:'34879rhfn98f7y5b t345rfjhnc5fhfhj895rh',
+          })
+        }
+
+        // Optionally, return true or false to control if the sign-in should be allowed
+        return true;
+      }
+      return false;
+    },
+  },
+
   secret: process.env.AUTH_SECRET,
 };
-
-
